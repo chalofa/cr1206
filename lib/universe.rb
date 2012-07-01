4723c31d77157f4673cc00f3dec2d781c9828965
Chalo Fernandez <chalofa@gmail.com>
2012-06-30 23:13:41 -0500

    Universe grid

--- lib/universe.rb
+++ lib/universe.rb
@@ -1,7 +1,18 @@
+require 'matrix'
+
 class Universe
-  attr_reader :dimension
+  attr_reader :dimension, :grid
 
   def initialize(dimension = 3)
     @dimension = dimension
+    @grid = Matrix.build(@dimension) { AliveCell.new }
+  end
+
+  def to_s
+    grid.row_size.times.reduce('') do |acum, index|
+      acum << grid.row(index).reduce('') do |text, cell|
+        text << cell.to_s
+      end + "\n"
+    end
   end
 end
--- spec/universe_spec.rb
+++ spec/universe_spec.rb
@@ -12,4 +12,25 @@ describe Universe do
     end
   end
 
+  describe 'grid' do
+    it 'should create the grid with alive cells' do
+      AliveCell.should_receive(:new).exactly(9)
+      Universe.new
+    end
+  end
+
+  describe '#to_s' do
+    context 'when default grid' do
+      it 'should display the grid cells state' do
+        subject.to_s.should == "ooo\nooo\nooo\n"
+      end
+    end
+
+    context 'when the smallest dimension' do
+      it 'should show the unique cell' do
+        Universe.new(1).to_s.should == "o\n"
+      end
+    end
+  end
+
 end
